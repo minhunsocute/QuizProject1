3b313b9296095da2c8293da4a8cca90d74dd1cd8
HƯNG NGUYỄN MINH <20120491@student.hcmus.edu.vn>
2021-12-16 01:33:23 +0700

    design desgin và design

--- QuizProject1/MainWindow.xaml.cs
+++ QuizProject1/MainWindow.xaml.cs
@@ -30,19 +30,97 @@ namespace QuizProject1
             //System.Windows.Application.Current.Shutdown();
         }
 
-        private void ListViewItem_PreviewMouseDoubleClick(object sender, MouseButtonEventArgs e)
-        {
-            System.Windows.Application.Current.Shutdown();
-        }
 
-        private void ShutDownListView_PreviewMouseDoubleClick(object sender, MouseButtonEventArgs e)
-        {
-            System.Windows.Application.Current.Shutdown();
-        }
-
+        #region events Click
         private void btnPower_Click(object sender, RoutedEventArgs e)
         {
             System.Windows.Application.Current.Shutdown();
         }
+
+        private void PackIcon_MouseEnter_1(object sender, MouseEventArgs e)
+        {
+            btnYTB.Background = Brushes.Transparent;
+            btnYTB.Foreground = Brushes.Black;
+        }
+       
+        private void btnTeaCher_MouseEnter(object sender, MouseEventArgs e)
+        {
+            btnTeaCher.Background = Brushes.Transparent;
+        }
+
+        private void btnShutDown_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+        private void ListViewItem_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if(tg_btn.IsChecked == true)
+            {
+                tl_teacher.Visibility = Visibility.Collapsed;
+                tl_student.Visibility = Visibility.Collapsed;
+                tl_admin.Visibility = Visibility.Collapsed;
+                tl_shutdown.Visibility = Visibility.Collapsed;  
+            }
+            else
+            {
+                tl_shutdown.Visibility = Visibility.Visible;
+                tl_student.Visibility= Visibility.Visible;  
+                tl_teacher.Visibility= Visibility.Visible;
+                tl_admin.Visibility= Visibility.Visible;
+            }
+        }
+        
+
+        private void tg_btn_Checked(object sender, RoutedEventArgs e)
+        {
+            nav_info.Opacity = 0.3;
+        }
+
+        private void tg_btn_Unchecked(object sender, RoutedEventArgs e)
+        {
+            nav_info.Opacity = 1;
+        }
+
+        private void btnTeaCher_Checked(object sender, RoutedEventArgs e)
+        {
+            teacherText.Foreground = Brushes.Black;
+            btnAdmin.IsChecked = false;
+            btnShutDown.IsChecked = false;
+            btnStudent.IsChecked = false;
+            tg_btn.IsChecked = false;
+        }
+        
+        private void btnTeaCher_Unchecked(object sender, RoutedEventArgs e)
+        {
+            teacherText.Foreground = Brushes.White;
+        }
+
+        private void btnStudent_Checked(object sender, RoutedEventArgs e)
+        {
+            studenttext.Foreground = Brushes.Black;
+            btnTeaCher.IsChecked = false;
+            btnShutDown.IsChecked=false;
+            btnAdmin.IsChecked=false;
+            tg_btn.IsChecked =false;
+        }
+
+        private void btnStudent_Unchecked(object sender, RoutedEventArgs e)
+        {
+            studenttext.Foreground= Brushes.White;  
+        }
+
+        private void btnAdmin_Unchecked(object sender, RoutedEventArgs e)
+        {
+            admintext.Foreground = Brushes.White;
+        }
+
+        private void btnAdmin_Checked(object sender, RoutedEventArgs e)
+        {
+            admintext.Foreground = Brushes.Black;
+            btnTeaCher.IsChecked=false;
+            btnStudent.IsChecked=false;
+            tg_btn.IsChecked =false;
+        }
+        #endregion
     }
 }
