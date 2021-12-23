19d1be6763465ea5ba956f0e3b06599587cdc291
HƯNG NGUYỄN MINH <20120491@student.hcmus.edu.vn>
2021-12-24 00:31:21 +0700

    Update

---
+++ QuizProject1/Views/student_mainForm/CustomControl/customListExam.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QuizProject1.CustomControl
+{
+    /// <summary>
+    /// Interaction logic for customListExam.xaml
+    /// </summary>
+    public partial class customListExam : UserControl
+    {
+        public customListExam()
+        {
+            InitializeComponent();
+            DataContext = new MyFilesViewModel();
+            
+        }
+        public PathGeometry FileThumb
+        {
+            get { return (PathGeometry)GetValue(FileThumbProperty); }
+            set { SetValue(FileThumbProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for FileThumb.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty FileThumbProperty =
+            DependencyProperty.Register("FileThumb", typeof(PathGeometry), typeof(customListExam));
+
+    }
+}
---
+++ QuizProject1/Views/student_mainForm/settingStudent_form.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QuizProject1.Views.student_mainForm
+{
+    /// <summary>
+    /// Interaction logic for settingStudent_form.xaml
+    /// </summary>
+    public partial class settingStudent_form : UserControl
+    {
+        public settingStudent_form()
+        {
+            InitializeComponent();
+        }
+    }
+}
